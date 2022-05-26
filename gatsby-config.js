4b66daba7672a31c8d299dedca89a9a8f5fe2489
su-goh <49136637+su-goh@users.noreply.github.com>
2022-05-26 03:00:08 -0500

    tons of changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
-    siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
+    siteTitle: `Fish thoughts`,
+    siteTitleAlt: `Fish thoughts`,
+    siteHeadline: `Big thoughts from a little fish`,
+    siteUrl: `https://haiimsean.gatsbyjs.io/`,
+    siteDescription: `Little thoughts from a fish`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `A little fish`,
   },
   plugins: [
     {
@@ -23,7 +23,7 @@ module.exports = {
       options: {
         navigation: [
           {
-            title: `Blog`,
+            title: `Blob`,
             slug: `/blog`,
           },
           {
@@ -31,16 +31,16 @@ module.exports = {
             slug: `/about`,
           },
         ],
-        externalLinks: [
-          {
-            name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
-          },
-          {
-            name: `Homepage`,
-            url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
-          },
-        ],
+        // externalLinks: [
+        //   {
+        //     name: `Twitter`,
+        //     url: `https://twitter.com/lekoarts_de`,
+        //   },
+        //   {
+        //     name: `Homepage`,
+        //     url: `https://www.lekoarts.de?utm_source=minimal-blog&utm_medium=Starter`,
+        //   },
+        // ],
       },
     },
     {
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,8 +3,8 @@ import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 
 const NotFound = () => (
   <Layout>
-    <h1>404 - Page Not Found</h1>
-    <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <h1>I don't know what you are trying to look for but</h1>
+    <p>It's not here. Now shoo</p>
   </Layout>
 )
 
